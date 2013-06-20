d433181e862e48a49a53135dd96cd17348c2a936
Carson Gross <carsongross@gmail.com>
2013-06-20 13:19:31 -0700

    Implement blog

--- _plugins/nav_tags.rb
+++ _plugins/nav_tags.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module Jekyll
   class ActiveTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
@@ -19,7 +21,19 @@ module Jekyll
       'hide' unless nav.include? @text.chop
     end
   end
+  class GravatarTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @expr = text.strip
+    end
+    def render(context)
+      email = context[@expr] || 'none'
+      hash = Digest::MD5.hexdigest(email)
+      "http://www.gravatar.com/avatar/#{hash}?s=32"
+    end
+  end
 end
 
 Liquid::Template.register_tag('active', Jekyll::ActiveTag)
 Liquid::Template.register_tag('unless', Jekyll::HideTag)
+Liquid::Template.register_tag('gravatar', Jekyll::GravatarTag)
--- _posts/2013-06-10-welcome-to-jekyll.md
+++ _posts/2013-06-10-welcome-to-jekyll.md
@@ -3,6 +3,7 @@ layout: post
 title:  "Welcome to Jekyll!"
 date:   2013-06-10 21:42:46
 categories: jekyll update
+author: brett@chrometa.com
 ---
 
 You'll find this post in your `_posts` directory - edit this post and re-build (or run with the `-w` switch) to see your changes!
