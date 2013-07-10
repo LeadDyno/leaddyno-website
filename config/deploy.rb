5c3feba3aabb771348a7a3626be36bb014127447
Carson Gross <carsongross@gmail.com>
2013-07-10 14:28:52 -0700

    Update images

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 require 'capistrano/s3'
 
 set :deployment_path, "_site"
-set :bucket, "leaddyno.com"
+set :bucket, "www.leaddyno.com"
 set :access_key_id, ENV['AWS_ACCESS_KEY']
 set :secret_access_key, ENV['AWS_SECRET_KEY']
 
